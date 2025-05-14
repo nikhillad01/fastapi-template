0cb645eb983ded8c81a758b84b8abee1a6e14cb8
Nikhil Lad <nikhillad01@gmail.com>
2025-05-14 17:29:45 +0530

    refactor: remove unnecessary code

--- app/models/user.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-from sqlalchemy import Column, Integer, String
-from app.database.db_session import Base
-
-class User(Base):
-    __tablename__= "users"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    
