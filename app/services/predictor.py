66c3383c95b013ba379662626b1b58c0035550d2
Nikhil Lad <nikhillad01@gmail.com>
2025-05-15 12:28:09 +0530

    integrate new model

--- app/services/predictor.py
+++ app/services/predictor.py
@@ -4,8 +4,8 @@ from app.schemas.prediction import HouseData
 
 class PredictorService:
     def __init__(self):
-        self.model_path = "ai_model/my_linear_regression.pkl"
-        self.scaler_path = "ai_model/my_dataframes.pkl"
+        self.model_path = "ai_model/new_model.pkl"
+        self.scaler_path = "ai_model/scaler.pkl"
 
         self.model = joblib.load(self.model_path)
         self.scaler = joblib.load(self.scaler_path)
