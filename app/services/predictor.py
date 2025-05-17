cba603bb17b40d7f80bad6418d9295a40067dcf8
Nikhil Lad <nikhillad01@gmail.com>
2025-05-17 09:13:06 +0530

    fix: filename

--- app/services/predictor.py
+++ app/services/predictor.py
@@ -5,7 +5,7 @@ from app.schemas import HouseDataInput, HouseDataInternal
 class PredictorService:
     def __init__(self):
         self.model_path = "ai_model/new_model.pkl"
-        self.scaler_path = "ai_model/scaler.pkl"
+        self.scaler_path = "ai_model/new_scaler.pkl"
 
         self.model = joblib.load(self.model_path)
         self.scaler = joblib.load(self.scaler_path)
